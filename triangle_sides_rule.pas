5c4078cf50164c80766a033549a2f69c59dde807
vitrubnikova <vitrubnikova@gmail.com>
2020-12-16 08:13:51 +0300

    mistakes correction

--- triangle_sides_rule.pas
+++ triangle_sides_rule.pas
@@ -9,7 +9,7 @@ begin
         readln (c);
         if (a <= 0) or (b <= 0) or (c <= 0) then
         writeln ('Пожалуйста, введите положительное число, отличное от нуля')
-        else if (a < b + c) and (b < a + c) and (c < b + c) then
+        else if (a < b + c) and (b < a + c) and (c < b + а) then
         writeln ('Треугольник получается')
         else writeln ('Треугольник не получается');
         readln;
